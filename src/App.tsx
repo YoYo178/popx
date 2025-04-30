df4347af2fc2c96511cc8dea815bf01386541cf9
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-30 18:17:39 +0530

    Configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { RootLayout } from './layouts/RootLayout/RootLayout'
+
+import { Landing } from './pages/Landing/Landing'
+import { Login } from './pages/Login/Login'
+import { Register } from './pages/Register/Register'
+import { Profile } from './pages/Profile/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter basename="/popx">
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<Landing />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
