63dbca64bab873fff4ef40f5c8ccf1d00a6db3a3
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-30 18:17:10 +0530

    Create login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,26 @@
+import { Button } from "../../components/Button/Button"
+import { Input } from "../../components/Input/Input"
+import './Login.css'
+
+export const Login = () => {
+  return (
+    <div className="login-modal">
+      <div className="login-title">
+        <p>Signin to your</p>
+        <p>PopX account</p>
+      </div>
+
+      <div className="description">
+        <p>Lorem ipsum dolor sit amet,</p>
+        <p>consectetur adipiscing elit.</p>
+      </div>
+
+      <div className="login-fields-container">
+        <Input labelColor='#6c25ff' labelText='Email address' placeholder='Enter email address' />
+        <Input labelColor='#6c25ff' labelText='Password' placeholder='Enter password' />
+      </div>
+
+      <Button bgColor="#cbcbcb" textColor="white" redirectPath="/profile">Login</Button>
+    </div>
+  )
+}
