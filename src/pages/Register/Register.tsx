d217ede46c2847491b8e5c5e55cea1a500e78f6e
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-30 18:17:03 +0530

    Create Register page

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,27 @@
+import { Button } from "../../components/Button/Button"
+import { Input } from "../../components/Input/Input"
+import { RadioButtonQuestion } from "../../components/RadioButtonQuestion/RadioButtonQuestion"
+import './Register.css'
+
+export const Register = () => {
+  return (
+    <div className="register-modal">
+      <div className="title">
+        <p>Create your</p>
+        <p>PopX account</p>
+      </div>
+
+      <div className="fields-container">
+        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Full Name' required />
+        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Phone number' required />
+        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Email address' required />
+        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Password' required />
+        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Company name' />
+
+        <RadioButtonQuestion name='agency-question' question="Are you an Agency?" options={['Yes', 'No']} required checked={[true, false]} />
+      </div>
+
+      <Button bgColor="#6c25ff" redirectPath="/profile" textColor="white">Create Account</Button>
+    </div>
+  )
+}
