0fe521de921e66a338c9f8b5dec1aeae093fb2f4
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-04-30 18:35:25 +0530

    Add titles to pages

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,23 +4,26 @@ import './Login.css'
 
 export const Login = () => {
   return (
-    <div className="login-modal">
-      <div className="login-title">
-        <p>Signin to your</p>
-        <p>PopX account</p>
-      </div>
+    <>
+      <title>PopX | Sign In </title>
+      <div className="login-modal">
+        <div className="login-title">
+          <p>Signin to your</p>
+          <p>PopX account</p>
+        </div>
 
-      <div className="description">
-        <p>Lorem ipsum dolor sit amet,</p>
-        <p>consectetur adipiscing elit.</p>
-      </div>
+        <div className="description">
+          <p>Lorem ipsum dolor sit amet,</p>
+          <p>consectetur adipiscing elit.</p>
+        </div>
 
-      <div className="login-fields-container">
-        <Input labelColor='#6c25ff' labelText='Email address' placeholder='Enter email address' />
-        <Input labelColor='#6c25ff' labelText='Password' placeholder='Enter password' />
-      </div>
+        <div className="login-fields-container">
+          <Input labelColor='#6c25ff' labelText='Email address' placeholder='Enter email address' />
+          <Input labelColor='#6c25ff' labelText='Password' placeholder='Enter password' />
+        </div>
 
-      <Button bgColor="#cbcbcb" textColor="white" redirectPath="/profile">Login</Button>
-    </div>
+        <Button bgColor="#cbcbcb" textColor="white" redirectPath="/profile">Login</Button>
+      </div>
+    </>
   )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -5,23 +5,26 @@ import './Register.css'
 
 export const Register = () => {
   return (
-    <div className="register-modal">
-      <div className="title">
-        <p>Create your</p>
-        <p>PopX account</p>
+    <>
+      <title>PopX | Register</title>
+      <div className="register-modal">
+        <div className="title">
+          <p>Create your</p>
+          <p>PopX account</p>
+        </div>
+
+        <div className="fields-container">
+          <Input labelColor='#6c25ff' value='Marry Doe' labelText='Full Name' required />
+          <Input labelColor='#6c25ff' value='Marry Doe' labelText='Phone number' required />
+          <Input labelColor='#6c25ff' value='Marry Doe' labelText='Email address' required />
+          <Input labelColor='#6c25ff' value='Marry Doe' labelText='Password' required />
+          <Input labelColor='#6c25ff' value='Marry Doe' labelText='Company name' />
+
+          <RadioButtonQuestion name='agency-question' question="Are you an Agency?" options={['Yes', 'No']} defaultCheck={0} required />
+        </div>
+
+        <Button bgColor="#6c25ff" redirectPath="/profile" textColor="white">Create Account</Button>
       </div>
-
-      <div className="fields-container">
-        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Full Name' required />
-        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Phone number' required />
-        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Email address' required />
-        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Password' required />
-        <Input labelColor='#6c25ff' value='Marry Doe' labelText='Company name' />
-
-        <RadioButtonQuestion name='agency-question' question="Are you an Agency?" options={['Yes', 'No']} defaultCheck={0} required />
-      </div>
-
-      <Button bgColor="#6c25ff" redirectPath="/profile" textColor="white">Create Account</Button>
-    </div>
+    </>
   )
 }
